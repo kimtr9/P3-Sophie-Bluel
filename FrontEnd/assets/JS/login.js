765d163f14ab899c1a1a1b31734303c8de418106
Kim <kim9truong@gmail.com>
2024-06-05 14:57:18 +0200

    ajout fichiers reorganisés

--- FrontEnd/assets/JS/login.js
+++ FrontEnd/assets/JS/login.js
@@ -1,13 +1,11 @@
+//Récupération du formulaire
 const loginForm = document.querySelector('.login-form')
-
-
+// Ecouteur d'évènement au moment du submit
 loginForm.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-
     const email = loginForm.querySelector("#email").value
     const password = loginForm.querySelector("#password").value
-
 
     // Requête POST à l'API
     try {
@@ -21,14 +19,17 @@ loginForm.addEventListener('submit', async (event) => {
         });
 
         const data = await response.json()
+
+        if(!response.ok) {
+            alert(data.message || 'Erreur dans l’identifiant ou le mot de passe');
+            return;
+        }
         console.log('Réponse: ', data);
-    
 
         // Stockage du token dans le localStorage
         localStorage.setItem("token", data.token)
         // redirection vers page d'accueil 
-        window.location.href = "./index.html";
-       
+        window.location.href = "./../../index.html";
         
     } catch (error) {
         console.log('Erreur: ', error)
