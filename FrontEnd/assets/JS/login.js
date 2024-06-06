e00af69083c309afee052141502e29ffb8cba1fb
kimtr9 <165020812+kimtr9@users.noreply.github.com>
2024-06-06 18:25:07 +0200

    mode édition

--- FrontEnd/assets/JS/login.js
+++ FrontEnd/assets/JS/login.js
@@ -19,12 +19,18 @@ loginForm.addEventListener('submit', async (event) => {
         });
 
         const data = await response.json()
+
+        if(!response.ok) {
+            alert(data.message || 'Erreur dans l’identifiant ou le mot de passe');
+            return;
+        }
         console.log('Réponse: ', data);
 
         // Stockage du token dans le localStorage
         localStorage.setItem("token", data.token)
         // redirection vers page d'accueil 
-        window.location.href = "../../index.html";
+        window.location.href = "index.html";
+        
         
     } catch (error) {
         console.log('Erreur: ', error)
